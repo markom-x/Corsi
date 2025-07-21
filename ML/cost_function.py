3453dcc832261b04634bcea0f243c07615784ffb
Markom.x <marcocarbonebdi@hotmail.it>
2025-07-21 17:26:18 +0200

    Cost Function working

--- ML/cost_function.py
+++ ML/cost_function.py
@@ -1,9 +1,7 @@
+import math, copy
 import numpy as np
 import matplotlib.pyplot as plt
-from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
-
-plt.style.use('./deeplearning.mplstyle')
-
+from lab_utils_uni import plt_house_x, plt_contour_wgrad, plt_divergence, plt_gradients
 x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
 y_train = np.array([250, 300, 480,  430,   630, 730,])
 
@@ -33,6 +31,36 @@ def compute_cost(x, y, w, b):
 
     return total_cost
 
+def compute_gradient(x, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    
+    # Number of training examples
+    m = x.shape[0]    
+    dj_dw = 0
+    dj_db = 0
+    
+    for i in range(m):  
+        f_wb = w * x[i] + b 
+        dj_dw_i = (f_wb - y[i]) * x[i] 
+        dj_db_i = f_wb - y[i] 
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i 
+    dj_dw = dj_dw / m 
+    dj_db = dj_db / m 
+        
+    return dj_dw, dj_db
+
+plt_gradients(x_train,y_train, compute_cost, compute_gradient)
+plt.show()
 
 
 
